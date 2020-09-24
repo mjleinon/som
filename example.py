44a05f4fd8c52f579802f9ee620a85075b386ca3
mjleinon <37039101+mjleinon@users.noreply.github.com>
2020-09-24 13:20:14 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,13 @@
 from som import *
+from plotsom import *
 
 k = SOMNetwork()
 k.get_data("iris.txt")
 k.train()
-print(k.strmap())
+
+"plot examples over dimension pairs:"
+p = plotsom.PlotSOM(k)
+p.plot_nodes()
+p.plot_nodes((1,2))
+p.plot_nodes((0,2))
+p.plot_nodes((0,3))
