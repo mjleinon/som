2bde3376d2eb1d61bfe101d6c8fc641286a8f6ab
mjleinon <37039101+mjleinon@users.noreply.github.com>
2020-09-24 13:25:57 +0300

    Update plotsom.py

--- plotsom.py
+++ plotsom.py
@@ -8,6 +8,22 @@ Created on Thu Sep 24 11:49:27 2020
 import matplotlib.pyplot as plt
 import numpy as np
 
+def arrayshow_flipped(data, label='colorMap'):
+    fig = plt.figure(figsize=(6, 3.2))
+    ax = fig.add_subplot(111)
+    ax.set_title(label)
+    plt.imshow(data, origin="upper")
+    ax.set_aspect('equal')
+    
+    cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+    cax.get_xaxis().set_visible(False)
+    cax.get_yaxis().set_visible(False)
+    cax.patch.set_alpha(0)
+    cax.set_frame_on(False)
+    plt.colorbar(orientation='vertical')
+    plt.show()
+
+
 class PlotSOM:
     
     def __init__(self, som):
