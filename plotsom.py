d79c6d4009c6c397ada60f5524c95b5ef1d49c41
mjleinon <37039101+mjleinon@users.noreply.github.com>
2020-09-24 13:23:38 +0300

    Update plotsom.py

--- plotsom.py
+++ plotsom.py
@@ -32,30 +32,5 @@ class PlotSOM:
         plt.scatter([n.val[ix] for n in self.som.iterate_nodes()],[n.val[iy] for n in self.som.iterate_nodes()],c="r",figure=fig)
                     
                 
-    def strmap(self,values=True):
-        "list of node vectors or input indices belonging to node vectors"
-        rstring = ""
-        for row in self.som.nodes:
-            srow = ""
-            for node in row:
-                if values:
-                    for dim in node.val:
-                        srow += " "+str(dim)+","
-                    srow += "; \n"    
-                else:
-                    for i in node.owns:
-                        srow += " "+str(i)+","
-                    srow += "; \n"    
-            rstring += " "+str(srow)
-            rstring += "-- \n"
-        return rstring 
-    
-    def table_map(self):
-        rstring = ""
-        for row in self.som.nodes:
-            srow = ""
-            for node in row:
-                pass
-    
     
     
