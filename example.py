5d1669c8864ef2966a04fea72f283693a0aade28
mjleinon <37039101+mjleinon@users.noreply.github.com>
2020-09-24 13:27:30 +0300

    Update example.py

--- example.py
+++ example.py
@@ -11,3 +11,11 @@ p.plot_nodes()
 p.plot_nodes((1,2))
 p.plot_nodes((0,2))
 p.plot_nodes((0,3))
+
+
+"u-matrix and ownermatrix of trained network:"
+k.create_u_matrix()
+show(k.u_matrix, label="U-matrix 25x25 SOM")
+
+k.create_owner_matrix()
+show(k.owner_matrix, label="OWN-matrix 25x25 SOM")
